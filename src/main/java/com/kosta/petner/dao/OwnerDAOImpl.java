8c69f3a875f9996aa1772ec35da2c70b8e0605c7
planet4670 <planet4670@gmail.com>
2022-11-15 16:46:25 +0900

    pom.xml 수정

--- src/main/java/com/kosta/petner/dao/OwnerDAOImpl.java
+++ src/main/java/com/kosta/petner/dao/OwnerDAOImpl.java
@@ -20,12 +20,11 @@ public class OwnerDAOImpl implements OwnerDAO {
 
 	}
 
-<<<<<<< HEAD
 	@Override // 보호자의 반려동물 정보가져오기 221115_DSC
 	public PetInfo getPetInfo(int user_no) {
 		return sqlSession.selectOne("mapper.owner.getPetInfo", user_no);
-=======
-	//user_no에 맞는 pet정보를 가져옴
+	}
+		//user_no에 맞는 pet정보를 가져옴
 	@Override
 	public List<PetInfo> getPetByUserNo(Integer user_no) {
 		return sqlSession.selectList("mapper.owner.getPetByUserNo", user_no);
@@ -34,7 +33,6 @@ public class OwnerDAOImpl implements OwnerDAO {
 	@Override
 	public PetInfo getPetByPetNo(Integer pet_no) {
 		return sqlSession.selectOne("mapper.owner.getPetByPetNo", pet_no);
->>>>>>> 2a3fdc56a2a53caf11d59b5eb1a1ea114c46341d
 	}
 
 }
