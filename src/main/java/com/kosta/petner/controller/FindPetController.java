6a62cee8988a29deb783ec19606f155503668e0d
KimHyeKyung <kimhye.1218@gmail.com>
2022-12-19 16:34:28 +0900

    돌봐줄 동물찾기 controller오류 수정 완료

--- src/main/java/com/kosta/petner/controller/FindPetController.java
+++ src/main/java/com/kosta/petner/controller/FindPetController.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,18 +42,49 @@ public class FindPetController {
 	@Autowired
 	SitterService sitterService;
 
-	// 돌봐줄 동물 찾기 페이지
-	@RequestMapping(value = "/findPet", method = { RequestMethod.POST, RequestMethod.GET })
-	String findPet(Model model, @RequestParam(value = "zipcode", required = false, defaultValue = "1") String qszipcode,
-			@RequestParam(value = "addr", required = false) String addr, Find findPetVO, HttpServletRequest request) {
-		// user의 DB에 저장된 값 가져오기
+	//돌봐줄 동물 찾기 페이지
+	@RequestMapping(value = "/findPet", method= {RequestMethod.POST, RequestMethod.GET})
+	String findPet(Model model, @RequestParam(value="zipcode", required=false, defaultValue ="1") String qszipcode,
+					@RequestParam(value="addr", required=false) String addr, Find findPetVO, HttpServletRequest request) {
+		//user의 DB에 저장된 값 가져오기
 		Users users = (Users) WebUtils.getSessionAttribute(request, "authUser");
+		if(users == null) {
+			System.out.println("findPet authUser null");
+		}else {
+			Integer user_no = users.getUser_no();
+			Users userInfo = usersService.getUserByUserNo(user_no);
+			model.addAttribute("userInfo", userInfo);
+		}
 		
+		//viewForm에서 넘어온 값 확인
+		System.out.println("*******findPet request.getParameter******");
+		String st_date = request.getParameter("st_date");
+		String end_date = request.getParameter("end_date");
+		String service = request.getParameter("service");
+		String pet_kind = request.getParameter("pet_kind");
+		String gender = request.getParameter("gender");
+		String zipcode = request.getParameter("zipcode");
+		String addrP = request.getParameter("addrP");
+		
+		System.out.println("st_date:  "+st_date);
+		System.out.println("end_date:  "+end_date);
+		System.out.println("service:  "+service);
+		System.out.println("pet_kind:  "+pet_kind);
+		System.out.println("gender:  "+gender);
+		System.out.println("zipcode:  "+zipcode);
+		System.out.println("addrP:  "+addrP);
+		System.out.println("**********************************************");
+		
+		model.addAttribute("qszipcode", qszipcode);
+		model.addAttribute("qsaddrp", addr);
+		model.addAttribute("title", "돌봐줄 동물 찾기");
+		model.addAttribute("page", "main/find/findPet");
 		return "/layout/main";
 	}
-
-	// 돌봐줄 동물 찾기 검색
-	// 검색조건 : 날짜, 서비스, 동물종류, 보호자 성별, (현재위치), 펫이름
+	
+	//돌봐줄 동물 찾기 검색
+	//검색조건 : 날짜, 서비스, 동물종류, 보호자 성별, (현재위치), 펫이름
+	@ResponseBody
 	@RequestMapping(value = "/findPet/viewForm/findPetSearch", method= RequestMethod.GET)
 	public List<CareService> findPetSearch(Model model, HttpServletRequest request) {
 		System.out.println("findPetSearch controller");
@@ -95,11 +125,11 @@ public class FindPetController {
 		}
 		return petSearchList;
 	}
-
-	// 게시글 사진 화면에 띄우기
+	
+	//게시글 사진 화면에 띄우기
 	@RequestMapping(value = "/findPet/{fileNo}", method = RequestMethod.GET)
 	public void viewCareServiceImages(@PathVariable String fileNo, HttpServletResponse response) {
-		String path = servletContext.getRealPath("/resources/upload/");
+	String path = servletContext.getRealPath("/resources/upload/");
 		FileInputStream fis = null;
 		try {
 			Integer file_no = Integer.parseInt(fileNo);
@@ -110,16 +140,13 @@ public class FindPetController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if (fis != null) {
+			if(fis != null) {
 				try {
 					fis.close();
-				} catch (Exception e) {
-				}
+				} catch (Exception e) {} 
 			}
 		}
 	}
-
-	
 		
 	//돌봐줄 동물 찾기 게시글에 따른 viewForm
 	@RequestMapping(value = "/findPet/viewForm/{serviceNo}", method= RequestMethod.GET)
